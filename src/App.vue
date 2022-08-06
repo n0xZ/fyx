3084974519b767fbf52796d24f9989bc96640702
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-08-06 18:06:02 -0300

    add vite-plugin-pages to project and set netlify deploy config
    Add VPP for handling routing without handling an single file with project's route, and set netlify config for building Fyx with PNPM.

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,5 @@
 </script>
 
 <template>
-	<section class="flex flex-col items-center mx-auto mt-12">
-		<h1>Bienvenido a: mi playground.</h1>
-		<button
-			class="py-2 px-5 w-24 rounded-xl border-amber-200 border-2"
-			@click="count++"
-		>
-			{{ count }}
-		</button>
-	<p>Doble : {{doubleCount}}</p>
-	</section>
+	<router-view />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+
 import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [Vue(), Pages()],
 	resolve: {
 		alias: {
 			'~': path.resolve('src'),
